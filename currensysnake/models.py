8a02887a9dd6032de2eed5a64b5c08d376182bab
romi477 <romi4240101@gmail.com>
2019-03-04 19:10:43 +0300

    just code

--- currensysnake/models.py
+++ currensysnake/models.py
@@ -4,13 +4,9 @@ from peewee import (SqliteDatabase, Model, IntegerField, DoubleField,
 
 db = SqliteDatabase('currency_snake.db')
 
-class BaseModel(Model):
+class XRate(Model):
     class Meta:
         database = db
-
-
-class XRate(BaseModel):
-    class Meta:
         db_table = 'xrates'
         indexes = (
             (('from_currency', 'to_currency'), True),
@@ -22,4 +18,11 @@ class XRate(BaseModel):
     updated = DateTimeField(default=peewee_datetime.datetime.now())
 
     def __repr__(self):
-        return f'XRate <{self.from_currency} --> {self.to_currency}, {self.rate}>'
+        return f'XRate <{self.from_currency} --> {self.to_currency}: {self.rate}>'
+
+
+def init_db():
+    db.drop_tables(XRate)
+    XRate.create_table()
+    XRate.create(from_currency=840, to_currency=980, rate=1)
+    print('db created!')
---
+++ currensysnake/test_api.py
@@ -0,0 +1,9 @@
+from models import XRate
+
+
+def update_xrates(from_currency, to_currency):
+    xrate = XRate.select().where(XRate.from_currency == from_currency, XRate.to_currency == to_currency).first()
+    print('rate before: ', xrate.rate)
+    xrate.rate += 0.01
+    xrate.save()
+    print('rate after: ', xrate.rate)
